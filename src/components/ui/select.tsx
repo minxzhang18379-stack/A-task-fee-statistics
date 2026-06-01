7d353aaffb4577ac79fbcc4c1e812d9705f512d8
minxzhang18379-stack <minxzhang18379@gmail.com>
2026-06-01 18:45:19 +0800

    优化部分元素设计

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-xl border-0 bg-card nm-btn px-3 py-2 text-sm data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 shadow-none transition-all duration-200",
+      "flex h-10 w-full items-center justify-between rounded-xl bg-card nm-btn px-3 py-2 text-sm data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 shadow-none transition-all duration-200",
       className
     )}
     {...props}
